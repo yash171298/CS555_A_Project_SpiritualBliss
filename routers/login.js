0b695c568db2a0e224ebbeb8cf5d7396d831b80a
Yash Patole <ypatole@stevens.edu>
2021-10-14 05:35:53 +0530

    Pushing login and sign up with routes & handlebar

---
+++ routers/login.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+const xss = require('xss');
+const data = require("../data");
+const loginData = data.login;
+const utils = require('../data/utils');
+
+router.get('/', async (req, res) => {
+    try {
+        res.render('posts/login', {});
+    } catch (e) {
+        res.status(500).json({
+            error: e
+        });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        let email;
+        let checkEmail = await utils.variableSanityCheck(xss(req.body.email), "string", "Email");
+        if (checkEmail.result)  email = (checkEmail.value).toLowerCase()
+        else{
+            res.status(400).render('posts/login', { error: "Incorrect user input", "message":"", title: "Mental and physical health-Yoga  - Login", notFound: false, userData:null})
+            return;
+        } 
+        
+        let password;
+        let checkPassword = await utils.variableSanityCheck(xss(req.body.password), "string", "Password");
+        if (checkPassword.result)  password = checkPassword.value
+        else{
+            res.status(400).render('posts/login', { error: "Incorrect user input", "message":"", title: "Mental and physical health-Yoga  - Login", notFound: false, userData:null})
+            return;
+        }
+
+        let logInUser = await loginData.login(xss(email), xss(password));
+        if (logInUser && logInUser.userData!=null) {            
+            req.session.user = {
+                firstName: logInUser.userData.firstName,
+                lastName: logInUser.userData.lastName,
+                isActive: logInUser.userData.isActive,
+                userType: logInUser.userData.userType,
+                userID: logInUser.userData._id,
+                universityDomain: logInUser.userData.email.substring(logInUser.userData.email.indexOf('@') + 1)
+            }
+           
+        }else{
+            res.status(404).render('posts/login', { error: "Incorrect email or password!", title: "Mental and physical health-Yoga  - Login",  notFound: false });
+        }
+        
+    } catch (e) {
+        res.status(e.statusCode?e.statusCode:500).render('posts/login', { error: e.error, title: "Mental and physical health-Yoga  - Login",  notFound: false })
+    }
+});
+
+module.exports = router;
---
+++ routers/signup.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const { users } = require('../data');
+const router = express.Router();
+const data = require("../data");
+const loginData = data.login;
+const xss = require('xss');
+
+router.get('/', async (req, res) => {
+    try {
+        res.render('posts/sign-up', {title: "Sign-up"});
+    } catch (e) {
+        res.status(500).json({
+            error: e
+        });
+    }
+});
+
+router.post('/', async (req, res) => {
+
+    try {
+        let flag = false;
+        if (!req.body.firstName) {
+            flag = true;
+            throw { "result": false, statusCode: 400, "message": "", error: "Please provide first name." };
+        }
+
+        if (!req.body.lastName) {
+            flag = true;
+            throw { "result": false, statusCode: 400, "message": "", error: "Please provide last name." }
+        }
+
+        if (!req.body.emailAddress) {
+            flag = true;
+            throw { "result": false, statusCode: 400, "message": "", error: "Please provide valid email address." }
+        }
+
+       
+
+        if (!req.body.password) {
+            flag = true
+            throw { "result": false, statusCode: 400, "message": "", error: "Please enter password." }
+        }
+
+       
+        let user = {
+            firstName: xss(req.body.firstName),
+            lastName: xss(req.body.lastName),
+            email: xss(req.body.emailAddress),
+            userType: xss(req.body.selectUserType),
+            password: xss(req.body.password),
+            universityName: xss(req.body.universityName),
+        }
+
+        const bloguserdata = user;
+        const { firstName, lastName, email, userType, password, universityName } = bloguserdata;
+        const newUser = await users.createnewuser(firstName, lastName, email, userType, password, universityName);
+
+        if (newUser == null) {
+            flag = true
+        }
+        if (!newUser) {
+            flag = true;
+        }
+        res.status(newUser.statusCode?newUser.statusCode:200).render('posts/SuccessMessage', { keyobject: newUser.data, message: newUser.message, error: newUser.error })
+    } catch (e) {
+        res.status(e.statusCode ? e.statusCode : 400).render('posts/sign-up', { message: e.message, error: e.error })
+    }
+});
+
+module.exports = router;
