682f21f7fae7edc17cae7e816eca56b2b0e742f9
Devika <mhatredevika@gmail.com>
2021-11-21 17:43:22 -0500

    branch issue push

---
+++ CS555_A_Project_SpiritualBliss-main/data/index.js
@@ -0,0 +1,13 @@
+const products = require("./products");
+const users = require("./users");
+const comments = require("./comments");
+const productType = require("./productType");
+const admin = require("./admin");
+
+module.exports = {
+  users,
+  comments,
+  products,
+  productType,
+  admin
+};
